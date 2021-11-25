74f2c8394ee21fbca34adee55d16c8c46b7eb35f
GitHub56TB <bt228beta@gmail.com>
2021-11-25 11:36:03 -0500

    Section 1-4 of Training

--- server/index1x.js
+++ server/index1x.js
@@ -1,26 +1,26 @@
 const http = require('http')
 
-const server = http.createServer((request, response) => {
-   
-  if (request.url === '/about'){
+ const server = http.createServer((request, response) => {
+   console.log(request.url)
+   if (request.url === '/about'){
 
     return response.end('THE ABOUT PAGE')
 
-  } else if (request.url === '/contact'){
+   } else if (request.url === '/contact'){
 
       return response.end('THE CONTACT PAGE')
 
-  } else if (request.url === '/'){
+    } else if (request.url === '/'){
 
       return response.end('THE HOME PAGE')
 
-  } else {
+    } else {
 
       response.writeHead(404)
 
       response.end('HTHE PAGE WAS NOT FOUND')
 
-  }
+    }
 
 
 
